4ff23bb7d1df93fa45cf7d4d85f3dafd01236311
alecpwills <alec.p.wills@gmail.com>
2018-05-02 20:51:10 -0400

    update report

--- project/outputs_correct/plot.py
+++ project/outputs_correct/plot.py
@@ -13,6 +13,14 @@ def e_pot(q, pts, ref):
     ref = np.array(ref)
     eo = 8.854187817e-12
     return q/ (np.linalg.norm(pts-ref)*4*np.pi*eo)
+
+def rho_strength(V, e=True):
+    eo = 8.854187817e-12
+    qe = 1.602e-19
+    if e==True:
+        return V*eo/qe
+    else:
+        return V*eo
 #%%
 # Use explicit one if in interactive shell, like in Spyder's IPython
 #dir_path = '/home/alec/Documents/Fortran/hwk5/
@@ -68,11 +76,12 @@ NY = int(LY/DY)
 xs = np.linspace(0, LX, NX)
 ys = np.linspace(0, LY, NY)
 plt.plot(xs[1:], 1/xs[1:])
+plt.zlim(0, 200)
 #%%
 lsts =[]
 for i in xs:
     for j in ys:
-        lsts.append([i, j, 1/np.linalg.norm(np.array([i-LX/2, j-LY/2]))])
+        lsts.append([i, j, rho_strength(-100)/np.linalg.norm(np.array([i-LX/2, j-LY/2]))])
 df = pd.DataFrame(lsts, columns=['x', 'y', 'z'])
 
 #%%
@@ -89,7 +98,7 @@ plt.plot(xs[1:], 1/xs[1:])
 lsts =[]
 for i in xs:
     for j in ys:
-        lsts.append([i, j, 100/np.linalg.norm(np.array([i-LX/2, j-LY/2]))])
+        lsts.append([i, j, 100/(4*np.pi*np.linalg.norm(np.array([i-LX/2, j-LY/2])))])
 df = pd.DataFrame(lsts, columns=['x', 'y', 'z'])
 f = plt.figure()
 ax = f.add_subplot(111)
@@ -110,14 +119,14 @@ elif length < 50000:
     size = 0.1
 else:
     size = 0.01
-cax = ax.scatter(xs=df.iloc[:, 0], ys=df.iloc[:, 1], zs=df.iloc[:, 2], marker='+', s=1, c=cols, cmap='inferno')
-#cax = ax.plot_trisurf(df.iloc[:, 0], df.iloc[:, 1], df.iloc[:, 2], cmap=cm.inferno,
-#                      antialiased=False, linewidth=0)
+#cax = ax.scatter(xs=df.iloc[:, 0], ys=df.iloc[:, 1], zs=df.iloc[:, 2], marker='+', s=1, c=cols, cmap='inferno')
+cax = ax.plot_trisurf(df.iloc[:, 0], df.iloc[:, 1], df.iloc[:, 2], cmap=cm.inferno,
+                      antialiased=False, linewidth=0)
 ax.set_ylabel('$y$ (cm)')
 ax.set_xlabel('$x$ (cm)')
 ax.set_zlabel('$\phi(x,y)$ (V)')
-ax.set_autoscalez_on(True)
-#ax.set_zlim(0, 1)
+#ax.set_autoscalez_on(True)
+ax.set_zlim(0, 100)
 cb = fig.colorbar(cax, orientation='horizontal', fraction=0.05)
 plt.tight_layout()
     
@@ -239,8 +248,101 @@ cax = ax.hexbin(x=df.iloc[:, 0], y=df.iloc[:, 1], C=df.iloc[:, 2], cmap='inferno
 cb = f.colorbar(cax)
 ax.set_ylabel('$y$ (cm)')
 ax.set_xlabel('$x$ (cm)')
-cb.set_label('$\phi(x,y)$ (V)')
+cb.set_label('$\Delta\phi(x,y)$ (V)')
 plt.tight_layout()
-plt.savefig('{}_heatmap.png'.format(file), dpi=800)
+plt.savefig('diffmat_heatmap.png'.format(file), dpi=800)
+plt.show()
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+cols = np.linspace(min(df.iloc[:, 2]), max(df.iloc[:, 2]), num=len(df.iloc[:, 2]))
+length = len(df.iloc[:, 2])
+if length < 1000:
+    size = 10
+elif length < 10000:
+    size = 1
+elif length < 50000:
+    size = 0.1
+else:
+    size = 0.01
+print(size)
+cax = ax.plot_trisurf(df.iloc[:, 0], df.iloc[:, 1], df.iloc[:, 2], cmap='inferno',
+                      antialiased=False, linewidth=1)
+ax.set_ylabel('$y$ (cm)')
+ax.set_xlabel('$x$ (cm)')
+ax.set_zlabel('$\Delta\phi(x,y)$ (V)')
+#ax.set_autoscalez_on(True)
+ax.set_zlim(-200, 200)
+ax.set_ylim(15, 0)
+cb = fig.colorbar(cax, orientation='horizontal', fraction=0.05)
+plt.tight_layout()
+plt.savefig('diffmat_graph.png'.format(file), dpi=800)
+plt.show()
+plt.close()
+#%%
+dir_path = '/home/alec/Documents/Fortran/project'
+diffmat = pd.read_table(os.path.join(dir_path, 'diffmat.dat'), header=None, delim_whitespace=True)
+sol = pd.read_table(os.path.join(dir_path, 'outputs_correct/three_bc.dat'), header=None, delim_whitespace=True)
+solsub = sol[(sol.iloc[:, 0] <= 40.0) & (sol.iloc[:, 1] <=15.0)]
+abc = pd.read_table(os.path.join(dir_path, 'analyticalmat.dat'), header=None, delim_whitespace=True)
+solsub = sol[(sol.iloc[:, 0] <= 40.0) & (sol.iloc[:, 1] <=15.0)]
+abcsub = abc[(abc.iloc[:, 0] <= 40.0) & (abc.iloc[:, 1] <=15.0)].iloc[:-1]
+
+LX = 40.1
+LY = 15.1
+DX = DY = 0.1
+NX = int(LX/DX)
+NY = int(LY/DY)
+xs = np.linspace(0, LX, NX+1)
+ys = np.linspace(0, LY, NY+1)
+#xs = np.linspace(0, LX, NX)
+#ys = np.linspace(0, LY, NY)
+lsts =[]
+for i in xs:
+    for j in ys:
+        if i == LX/2 or j == LY/2:
+            lsts.append([i, j, 100/(4*np.pi*np.linalg.norm(np.array([i-(LX+0.005)/2, j-(LY+0.005)/2])))])
+        else:
+            lsts.append([i, j, 100/(4*np.pi*np.linalg.norm(np.array([i-LX/2, j-LY/2])))])
+pt = pd.DataFrame(lsts, columns=['x', 'y', 'z'])
+#%%
+diffs = np.array(abc.iloc[:, 2]) + np.array(pt.iloc[:, 2])-np.array(sol.iloc[:, 2])
+f = plt.figure()
+ax = f.add_subplot(111)
+cax = ax.hexbin(x=sol.iloc[:, 0], y=sol.iloc[:, 1], C=diffs, cmap='inferno')
+cb = f.colorbar(cax)
+ax.set_ylabel('$y$ (cm)')
+ax.set_xlabel('$x$ (cm)')
+cb.set_label('$\Delta\phi(x,y)$ (V)')
+plt.tight_layout()
+plt.savefig('ptdiff_heat.png', dpi=800)
+plt.show()
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+cols = np.linspace(min(diffs), max(diffs), num=len(diffs))
+length = len(df.iloc[:, 2])
+if length < 1000:
+    size = 10
+elif length < 10000:
+    size = 1
+elif length < 50000:
+    size = 0.1
+else:
+    size = 0.01
+print(size)
+cax = ax.plot_trisurf(sol.iloc[:, 0], sol.iloc[:, 1], diffs, cmap='inferno',
+                      antialiased=False, linewidth=1)
+ax.set_ylabel('$y$ (cm)')
+ax.set_xlabel('$x$ (cm)')
+ax.set_zlabel('$\Delta\phi(x,y)$ (V)')
+#ax.set_autoscalez_on(True)
+ax.set_zlim(-200, 200)
+ax.set_ylim(15, 0)
+cb = fig.colorbar(cax, orientation='horizontal', fraction=0.05)
+plt.tight_layout()
+plt.savefig('ptdiff_graph.png'.format(file), dpi=800)
 plt.show()
 plt.close()
